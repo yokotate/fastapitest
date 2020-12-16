93dfca67b28100da0336358bb4ebe62910e0fb1e
yokosekazuya-at-055871665765 <yokosekazuya@yahoo.co.jp>
2020-12-16 18:46:18 +0900

    test

--- appengine/src/controllers.py
+++ appengine/src/controllers.py
@@ -23,24 +23,54 @@ jinja_env = templates.env  # Jinja2.Environment : filterやglobalの設定用
 
 def index(request: Request):
     #BigQueryにクエリを投げる
-    query_job = bigquery_client.query(
+    AFRO = bigquery_client.query(
         """
-        SELECT
-            *
-        FROM 
-            `COVID19.test`
-        ORDER BY 
-            DATE
+            SELECT date_reported, who_region,SUM(new_cases) as day, who_region as region  FROM COVID19.who where who_region = 'AFRO' group by date_reported, who_region order by date_reported;
+        """
+    )
+    AMRO = bigquery_client.query(
+        """
+            SELECT date_reported, who_region,SUM(new_cases) as day, who_region as region  FROM COVID19.who where who_region = 'AMRO' group by date_reported, who_region order by date_reported;
+        """
+    )
+    WPRO = bigquery_client.query(
+        """
+            SELECT date_reported, who_region,SUM(new_cases) as day, who_region as region  FROM COVID19.who where who_region = 'WPRO' group by date_reported, who_region order by date_reported;
+        """
+    )
+    EMRO = bigquery_client.query(
+        """
+            SELECT date_reported, who_region,SUM(new_cases) as day, who_region as region  FROM COVID19.who where who_region = 'EMRO' group by date_reported, who_region order by date_reported;
+        """
+    )
+    SEARO = bigquery_client.query(
+        """
+            SELECT date_reported, who_region,SUM(new_cases) as day, who_region as region  FROM COVID19.who where who_region = 'SEARO' group by date_reported, who_region order by date_reported;
+        """
+    )
+    EURO = bigquery_client.query(
+        """
+            SELECT date_reported, who_region,SUM(new_cases) as day, who_region as region  FROM COVID19.who where who_region = 'EURO' group by date_reported, who_region order by date_reported;
         """
     )
 
 
     # クエリの実行結果をデータフレームに取得する
-    df = query_job.to_dataframe()
+    df_AFRO = AFRO.to_dataframe()
+    df_AMRO = AMRO.to_dataframe()
+    df_WPRO = WPRO.to_dataframe()
+    df_EMRO = EMRO.to_dataframe()
+    df_SEARO = SEARO.to_dataframe()
+    df_EURO = EURO.to_dataframe()
     
-    labels = df["date"]
-    datas = [df["pcr_positive_daily"], df["pcr_positive_total"], df["death_total"]]
+    labels = df["datdate_reportede"]
+    datas_AFRO = [df_AFRO["date_reported"], df_AFRO["day"], df_AFRO["who_region"]]
+    datas_AMRO = [df_AMRO["date_reported"], df_AMRO["day"], df_AMRO["who_region"]]
+    datas_WPRO = [df_WPRO["date_reported"], df_WPRO["day"], df_WPRO["who_region"]]
+    datas_EMRO = [df_EMRO["date_reported"], df_EMRO["day"], df_EMRO["who_region"]]
+    datas_SEARO = [df_SEARO["date_reported"], df_SEARO["day"], df_SEARO["who_region"]]
+    datas_EURO = [df_EURO["date_reported"], df_EURO["day"], df_EURO["who_region"]]
 
 
-    return templates.TemplateResponse('index.html', {'request': request, 'datas':datas, 'labels':labels})
+    return templates.TemplateResponse('index.html', {'request': request, 'datas_AFRO':datas_AFRO, 'datas_AMRO':datas_AMRO, 'datas_WPRO':datas_WPRO, 'datas_EMRO':datas_EMRO, 'datas_SEARO':datas_SEARO, 'datas_EURO':datas_EURO, 'labels':labels})
 
