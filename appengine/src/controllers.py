85c572845f83440d9e9f50bec684a9391692f9f3
yokosekazuya-at-055871665765 <yokosekazuya@yahoo.co.jp>
2020-12-16 10:26:37 +0900

    Chart.jsの追加

--- appengine/src/controllers.py
+++ appengine/src/controllers.py
@@ -5,13 +5,14 @@ from starlette.templating import Jinja2Templates  # new
 from starlette.requests import Request
 from starlette.status import HTTP_401_UNAUTHORIZED
 
-import hashlib, secrets, re
+from google.cloud import bigquery
 
 app = FastAPI(
     title="ToDo App",
     description="FastAPIで作成するToDoアプリケーション",
     versions="0.9 beta"
 )
+bigquery_client = bigquery.Client()
 
 security = HTTPBasic()
 
@@ -21,5 +22,25 @@ templates = Jinja2Templates(directory="templates")
 jinja_env = templates.env  # Jinja2.Environment : filterやglobalの設定用
 
 def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+    #BigQueryにクエリを投げる
+    query_job = bigquery_client.query(
+        """
+        SELECT
+            *
+        FROM 
+            `COVID19.MHLW_JAPAN`
+        ORDER BY 
+            DATE
+        """
+    )
+
+
+    # クエリの実行結果をデータフレームに取得する
+    df = query_job.to_dataframe()
+    
+    labels = df["date"]
+    datas = [df["pcr_positive_daily"], df["pcr_tested_daily"], df["cases_total"], df["death_total"]]
+
+
+    return templates.TemplateResponse('index.html', {'request': request, 'datas':datas, 'labels':labels})
 
